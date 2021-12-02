284f0d13fd663efedbbdf5ee0a1279b5cb6a6ae3
dani-mashasha <dmmashasha@gmail.com>
2021-12-02 18:32:34 +0200

    table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar.js'
+import CryptoTable from './components/CryptoTable/CryptoTable.js'
+import Footer from './components/Footer/Footer.js'
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useEffect, useState } from 'react';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#455a64',
+        main: '#263238',
+        dark: '#000',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '##ffcc80',
+        main: '##ffa726',
+        dark: '##ff6d00',
+        contrastText: '#000',
+      },
+    },
+  });
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>crypto-app</h1>
 
+  const [coins, setCoins] = useState([])
+
+useEffect(() => {
+   fetch(`https://cors-anywhere.herokuapp.com/https://api.coinranking.com/v2/coins`, {
+    method: "GET",
+    headers: {
+      "Content-Type" : "application/json",
+      "x-access-token": "coinranking5d559847204aab210812637bbeeef168d3bf0de6f2d29bbc",
+      "Access-Control-Allow-Origin": "*"
+    }
+  }).then((response) => {
+    if(response.ok){
+      response.json().then((json)=> {
+        console.log(json.data.coins[0])
+        setCoins(json.data.coins)})
+    }
+  }).catch((error)=> console.log(error))
+
+}, [])
+
+
+
+  return (
+    <ThemeProvider theme={theme}>
+
+    <div className="App">
+      <Navbar />
+      <CryptoTable coins = {coins}/>
+      <Footer />
     </div>
+    
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+import { Grid } from "@mui/material";
+
+const Footer = () => {
+    return(
+<Grid container spacing={2} style={{backgroundColor:"primary"}}>
+  <Grid item xs={4} >
+      <p>jjjj</p>
+  </Grid>
+  <Grid item xs={4}>
+  <p>jjjj</p>
+
+  </Grid>
+  <Grid item xs={4}>
+  <p>jjjj</p>
+
+  </Grid>
+  
+</Grid>
+    )
+}
+export default Footer;
